cab6d283d906245b9fb969750f89a5a02ea09b9e
JBNAMAN <122195326+JBNAMAN@users.noreply.github.com>
2024-03-12 15:17:00 +0530

    my first commit

---
+++ aman.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class aman {
+public static void main(String[] args) {
+
+Scanner scanner = new Scanner(System.in);
+System.out.println("Enter no. : ");
+int number = scanner.nextInt();
+System.out.println(" The no. is  "+number);
+
+}
+
+}
