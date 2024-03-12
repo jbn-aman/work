363be35238148ba3bd26024f78f57e9a02cf371f
JBNAMAN <122195326+JBNAMAN@users.noreply.github.com>
2024-03-12 15:41:05 +0530

    Datatypedemo

---
+++ corejava/DataTypesDemo.java
@@ -0,0 +1,13 @@
+class DataTypesDemo {
+  public static void main(String[] args) {
+      byte b=10;
+      int i= 500;
+      double d= 10.5;
+      char a = 'a;
+      System.out.println("B = "+b);
+      System.out.println("D = "+d);
+      System.out.println("I = "+i);
+
+      System.out.println("A = "+a);
+      }
+}
